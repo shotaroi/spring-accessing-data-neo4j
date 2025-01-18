717e4a47aa5d96deb2e9277dc51cd1ac654aebd7
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-19 00:35:03 +0100

    Update Person.java

--- spring-accessing-data-neo4j/src/main/java/com/shotaroi/springaccessingdataneo4j/Person.java
+++ spring-accessing-data-neo4j/src/main/java/com/shotaroi/springaccessingdataneo4j/Person.java
@@ -1,4 +1,49 @@
 package com.shotaroi.springaccessingdataneo4j;
 
+import java.util.HashSet;
+
+@Node
 public class Person {
+    @Id @GeneratedValue private Long id;
+
+    private String name;
+
+    private Person() {
+        // Empty constructor required as of Neo4j API 2.0.5
+    };
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Neo4j doesn't REALLY have bi-directional relationships. It just means when querying
+     * to ignore the direction of the relationship.
+     * https://dzone.com/articles/modelling-data-neo4j
+     */
+    @Relationship(type = "TEAMMATE")
+    public Set<Person> teammates;
+
+    public void worksWith(Person person) {
+        if (teammates == null) {
+            teammates = new HashSet<>();
+        }
+        teammates.add(person);
+    }
+
+    public String toString() {
+        return this.name + "'s teammates => "
+                + Optional.ofNullable(this.teammates).orElse(
+                        Collections.emptySet()).stream()
+                .map(Person::getName)
+                .collect(Collectors.toList());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
