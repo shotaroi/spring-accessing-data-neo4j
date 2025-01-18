aed487cca3ed9f31416d1c860b87c006fde406d1
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-19 00:26:22 +0100

    Create project

---
+++ spring-accessing-data-neo4j/src/main/java/com/shotaroi/springaccessingdataneo4j/SpringAccessingDataNeo4jApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springaccessingdataneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAccessingDataNeo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAccessingDataNeo4jApplication.class, args);
+    }
+
+}
