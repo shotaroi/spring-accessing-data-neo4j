fd7365fd700eb379b07fae15f86183fce26a0530
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-20 00:21:35 +0100

    Update PersonRepository.java

--- spring-accessing-data-neo4j/src/main/java/com/shotaroi/springaccessingdataneo4j/PersonRepository.java
+++ spring-accessing-data-neo4j/src/main/java/com/shotaroi/springaccessingdataneo4j/PersonRepository.java
@@ -1,4 +1,10 @@
 package com.shotaroi.springaccessingdataneo4j;
 
-public interface PersonRepository {
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+import java.util.List;
+
+public interface PersonRepository extends Neo4jRepository<Person, Long> {
+    Person findByName(String name);
+    List<Person> findByTeammatesName(String name);
 }
